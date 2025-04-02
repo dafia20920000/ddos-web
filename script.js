561984f512bc16672f9a9f69db21c8ddc66b1737
dafia20920000 <gmgarena073@gmail.com>
2025-04-02 12:42:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function getOS() {
     if (userAgent.includes("Windows")) os = "Windows";
     else if (userAgent.includes("Mac")) os = "MacOS";
     else if (userAgent.includes("Linux")) os = "Linux";
-    else if (/Android/.test(userAgent)) os = "Android";
-    else if (/iPhone|iPad|iPod/.test(userAgent)) os = "iOS";
+    else if (/Android/.test(userAgent)) os = "Android " + navigator.userAgent.match(/Android (\d+\.\d+)/)[1]; // Menambahkan versi Android
+    else if (/iPhone|iPad|iPod/.test(userAgent)) os = "iOS " + navigator.userAgent.match(/OS (\d+_\d+)/)[1].replace('_', '.'); // Versi iOS
     document.getElementById('os').innerText = os;
 }
 
@@ -27,10 +27,16 @@ function getBatteryStatus() {
 
 function getDeviceModel() {
     let userAgent = navigator.userAgent;
-    let model = userAgent.match(/(.*?)/);
-    document.getElementById('device').innerText = model ? model[1] : "Tidak Diketahui";
+    let model = "Tidak Diketahui";
+    if (/Android/.test(userAgent)) {
+        model = userAgent.match(/Android.*; (.*?) Build/)[1] || "Model tidak diketahui";
+    } else if (/iPhone|iPad|iPod/.test(userAgent)) {
+        model = "Apple " + userAgent.match(/iPhone|iPad|iPod (.*) like/)[1];
+    }
+    document.getElementById('device').innerText = model;
 }
 
+// Fungsi untuk membuat PDF dengan link pemicu
 function downloadPDF() {
     const { jsPDF } = window.jspdf;
     let doc = new jsPDF();
@@ -40,6 +46,14 @@ function downloadPDF() {
     doc.text("Model HP: " + document.getElementById('device').innerText, 10, 40);
     doc.text("Status Baterai: " + document.getElementById('battery').innerText, 10, 50);
     doc.text("Status Web: Online", 10, 60);
+    
+    // Link yang akan mengirim informasi ke server saat diklik
+    let link = "https://yourserver.com/submit_info?device=" + encodeURIComponent(document.getElementById('device').innerText);
+    doc.text("Klik untuk Kirim Info ke Server (Data akan langsung dikirim): " + link, 10, 70);
+    
+    // Simulasi PDF rusak setelah link diklik
+    doc.text("File ini tidak bisa dibuka lagi setelah diklik.", 10, 80);
+    
     doc.save("admin_report.pdf");
 }
 
