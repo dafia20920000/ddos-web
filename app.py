4fba3cf02c1c497ce49d78c316538c8a8abde950
dafia20920000 <gmgarena073@gmail.com>
2025-04-02 12:50:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ app = Flask(__name__)
 
 @app.route('/submit_info', methods=['GET'])
 def submit_info():
+    # Menerima parameter 'device' dan 'ip' yang dikirim melalui URL
     device_info = request.args.get('device')
-    # Di sini, kamu bisa menyimpan atau menampilkan informasi perangkat ke panel admin
-    print(f"Received Device Info: {device_info}")
-    return "Info telah diterima!", 200
+    ip_info = request.args.get('ip')
+
+    if device_info and ip_info:
+        # Menampilkan informasi perangkat yang diterima
+        print(f"Received Device Info: {device_info}, IP: {ip_info}")
+        return "Info perangkat telah diterima!", 200
+    else:
+        return "Informasi perangkat tidak ditemukan.", 400
 
 if __name__ == "__main__":
     app.run(debug=True)
